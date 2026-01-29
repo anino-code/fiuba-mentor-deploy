72847b5f7363771c18206fe6d9c7fdb4f2607f36
Esteban <eordonez@fi.uba.ar>
2026-01-29 00:40:18 -0300

    Se cambio hacia el puerto 3030 y se rotaron los endpoints de users y forms en el index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,87 +11,86 @@ app.get("/api", (req, res) => {
   res.json({ status: 'OK'});
 });
 
-//GET. /FORMULARIOS
-app.get("/api/forms", (req, res) => {
-
-});
-
-//GET. /FORMULARIOS/<NOMBRE>
-app.get("/api/forms/:id_form", (req, res) => {
-
-});
-
-//POST. /FORMULARIOS
-app.post("/api/forms", (req, res) => {
-
-});
-
-//DELETE. /FORMULARIOS/<NOMBRE>
-app.delete("/api/forms/:id_form", (req, res) => {
-
-});
-
-//si uso pathch no necesito mandarle todo para actualizar, con put si
-//PUT. /FORMULARIOS/<NOMBRE>
-app.put("/api/forms/:id_form", (req, res) => {
-
-});
-
 //GET. /USUARIOS
 app.get("/api/users", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 //GET. /USUARIOS/<NOMBRE>
 app.get("/users/:id_user", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 //POST. /USUARIOS
 app.post("/api/users", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 
 //DELETE. /USUARIOS/<NOMBRE>
 app.delete("/api/users/:id_user", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 //si uso pathch no necesito mandarle todo para actualizar, con put si
 //PUT. /USUARIOS/<NOMBRE>
 app.put("/api/users/:id_user", (req, res) => {
+  res.json({ status: 'OK'});
+});
 
+//GET. /FORMULARIOS
+app.get("/api/forms", async (req, res) => {
+  res.json({ status: 'OK'});
+});
+
+//GET. /FORMULARIOS/<NOMBRE>
+app.get("/api/forms/:id_form", (req, res) => {
+  res.json({ status: 'OK'});
+});
+
+//POST. /FORMULARIOS
+app.post("/api/forms", (req, res) => {
+  res.json({ status: 'OK'});
+});
+
+//DELETE. /FORMULARIOS/<NOMBRE>
+app.delete("/api/forms/:id_form", (req, res) => {
+  res.json({ status: 'OK'});
+});
+
+//si uso pathch no necesito mandarle todo para actualizar, con put si
+//PUT. /FORMULARIOS/<NOMBRE>
+app.put("/api/forms/:id_form", (req, res) => {
+  res.json({ status: 'OK'});
 });
 
 //GET. /REVIEWS
 app.get("/api/reviews", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 //GET. /REVIEWS/<NOMBRE>
 app.get("/api/reviews/:id_review", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 //POST. /REVIEWS
 app.post("/api/reviews", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
-
 
 //DELETE. /REVIEWS/<NOMBRE>
 app.delete("/api/reviews/:id_review", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
 //si uso pathch no necesito mandarle todo para actualizar, con put si
 //PUT. /REVIEWS/<NOMBRE>
 app.put("/api/reviews/:id_review", (req, res) => {
-
+  res.json({ status: 'OK'});
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en http://localhost:" + PORT);
 });
